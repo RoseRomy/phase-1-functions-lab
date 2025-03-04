73eb56de2d66b5b61588094e56d67e4caeea621a
+ <RoseKamau@Romys-Macboook-Air.local>
2025-03-04 10:55:42 +0300

    Addes index.js

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+    const hqLocation = 42; // Headquarters is at block 42
+    return Math.abs(someValue - hqLocation);
+}
+function distanceFromHqInFeet(someValue) {
+    return distanceFromHqInBlocks(someValue) * 264;
+}
+function distanceTravelledInFeet(start, destination) {
+    return Math.abs(start - destination) * 264;
+}
+function calculatesFarePrice(start, destination) {
+    const distance = distanceTravelledInFeet(start, destination);
+    if (distance <= 400) {
+        return 0; // Free ride for first  400 feet
+    } else if (distance > 400 && distance <= 2000) {
+        return (distance - 400) * 0.02; // 2 cents per foot
+    } else if (distance > 2000 && distance <= 2500) {
+        return 25; // Flat fare for distances between 2000 and 2500 feet
+    } else {
+        return 'cannot travel that far'; // Over 2500 feet not allowed 
+    }
+}
